5165e2b49d6579562370fae4b1564f611e953273
Rogier Wachters <r@wachters.eu>
2025-06-22 04:15:57 +0200

    Refactor `GroupServiceImpl` imports and enhance error handling with `withError` for `DeleteGroupError`.

--- server/src/main/kotlin/eu/torvian/chatbot/server/service/core/impl/GroupServiceImpl.kt
+++ server/src/main/kotlin/eu/torvian/chatbot/server/service/core/impl/GroupServiceImpl.kt
@@ -1,14 +1,17 @@
 package eu.torvian.chatbot.server.service.core.impl
-import arrow.core.*
-import arrow.core.raise.*
-import arrow.core.raise.ensure
 
+import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import arrow.core.raise.withError
 import eu.torvian.chatbot.common.models.ChatGroup
 import eu.torvian.chatbot.server.data.dao.GroupDao
 import eu.torvian.chatbot.server.data.dao.SessionDao
 import eu.torvian.chatbot.server.data.dao.error.GroupError
 import eu.torvian.chatbot.server.service.core.GroupService
-import eu.torvian.chatbot.server.service.core.error.group.*
+import eu.torvian.chatbot.server.service.core.error.group.CreateGroupError
+import eu.torvian.chatbot.server.service.core.error.group.DeleteGroupError
+import eu.torvian.chatbot.server.service.core.error.group.RenameGroupError
 import eu.torvian.chatbot.server.utils.transactions.TransactionScope
 
 /**
@@ -55,7 +58,7 @@ class GroupServiceImpl(
             either {
                 // If the group exists, ungroup sessions.
                 sessionDao.ungroupSessions(id)
-                
+
                 withError({ daoError: GroupError.GroupNotFound ->
                     DeleteGroupError.GroupNotFound(daoError.id)
                 }) {
