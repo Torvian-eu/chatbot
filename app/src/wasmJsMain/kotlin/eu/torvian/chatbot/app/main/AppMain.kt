9415da14af2e4192aec658d1b9cbc0d38498387d
Rogier Wachters <r@wachters.eu>
2025-12-03 19:25:42 +0100

    chore(wasm): update appmodule parameter in wasmjsmain
    - Pass full `appConfig` object to `appModule`
    - Enables `appModule` to access all configuration properties

--- app/src/wasmJsMain/kotlin/eu/torvian/chatbot/app/main/AppMain.kt
+++ app/src/wasmJsMain/kotlin/eu/torvian/chatbot/app/main/AppMain.kt
@@ -39,7 +39,7 @@ fun main() {
         KoinApplication(application = {
             modules(
                 wasmJsModule(appConfig, encryptionConfig),
-                appModule(appConfig.serverUrl)
+                appModule(appConfig)
             )
         }) {
             AppShell()
