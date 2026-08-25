dc291486911960b75ecf37a19c0392560dae41aa
Rogier Wachters <r@wachters.eu>
2026-08-25 12:38:33 +0200

    chore(build): release version 0.10.0
    - Set the project version from snapshot to the 0.10.0 release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Properties
 // Global Project Metadata
 // ==========================================================
 
-val projectVersion = "0.10.0-SNAPSHOT"
+val projectVersion = "0.10.0"
 val projectName = "chatbot"
 val projectGroup = "eu.torvian"
 
